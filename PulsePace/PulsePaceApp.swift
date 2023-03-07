581b9f2c6ddd4f50cde862c5c57fbc1f09f0e70d
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-07 19:03:06 +0800

    Initial Commit

---
+++ PulsePace/PulsePaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsePaceApp.swift
+//  PulsePace
+//
+//  Created by Charisma Kausar on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PulsePaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
