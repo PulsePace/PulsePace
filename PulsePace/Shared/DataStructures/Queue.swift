448d87e4a097dac20a0d42d1cf928fa09ce6f27c
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-14 01:26:15 +0800

    Create data structure skeleton files

---
+++ PulsePace/Shared/DataStructures/Queue.swift
@@ -0,0 +1,17 @@
+//
+//  Queue.swift
+//  PulsePace
+//
+//  Created by Charisma Kausar on 14/3/23.
+//
+
+public protocol Queue {
+    associatedtype T
+    mutating func enqueue(_item: T)
+    mutating func dequeue() -> T?
+    mutating func removeAll()
+    func peek() -> T?
+    func toArray() -> [T]
+    var count: Int { get }
+    var isEmpty: Bool { get }
+}
