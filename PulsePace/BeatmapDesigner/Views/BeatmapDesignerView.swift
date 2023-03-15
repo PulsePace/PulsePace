ee59ba491bbf0f4340467ffd76e612e33513f2f7
petermonky <wjdgustjr0554@gmail.com>
2023-03-16 00:19:56 +0800

    Refactoring and UI improvements

--- PulsePace/BeatmapDesigner/Views/BeatmapDesignerView.swift
+++ PulsePace/BeatmapDesigner/Views/BeatmapDesignerView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import AVKit
 
 struct BeatmapDesignerView: View {
+    @EnvironmentObject var audioManager: AudioManager
     @StateObject var viewModel: BeatmapDesignerViewModel
 
     init(viewModel: BeatmapDesignerViewModel) {
@@ -17,133 +18,26 @@ struct BeatmapDesignerView: View {
 
     var body: some View {
         VStack {
-            VStack {
-                Slider(value: $viewModel.offset, in: 0...10)
-                Slider(value: $viewModel.zoom, in: 0...1_000)
-                Slider(value: $viewModel.divisorIndex, in: 0...Double(viewModel.divisorList.count - 1), step: 1)
-                if let player = viewModel.audioManager.player {
-                    renderSlider(player: player)
-                    renderPlaybackButtons(player: player)
-                }
+            HStack {
+                ZoomButtonsView()
+                TimelineView()
+                DivisorSliderView()
             }
-            .zIndex(.infinity)
 
-            ZStack {
-                ForEach(viewModel.hitObjects.toArray(), id: \.id) { hitObject in
-                    renderHitObject(hitObject)
-                }
-            }
-            .frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity,
-                alignment: .topLeading
-            )
-            .background(.black)
-            .onTapGesture { position in
-                let interval = 1 / (viewModel.bps * viewModel.divisor)
-                let beat = ((viewModel.sliderValue - viewModel.offset) / interval).rounded()
-                viewModel.hitObjects.enqueue(TapHitObject(position: position, beat: beat * interval))
-            }
+            CanvasView()
+
+            PlaybackControlView()
         }
         .onAppear {
-            viewModel.audioManager.startPlayer(track: "test")
+            audioManager.startPlayer(track: "test")
+            if let player = audioManager.player {
+                viewModel.initialisePlayer(player: player)
+            }
         }
         .onDisappear {
-            viewModel.audioManager.stopPlayer()
+            audioManager.stopPlayer()
         }
-    }
-
-    func renderSlider(player: AVAudioPlayer) -> some View {
-        ZStack {
-            GeometryReader { geometry in
-                let numberOfBeats: CGFloat = player.duration * viewModel.bps
-                let height = geometry.size.height
-                let mainBeatSpacing = viewModel.zoom / viewModel.bps
-                let subBeatSpacing = mainBeatSpacing / viewModel.divisor
-
-                Path { path in
-                    for index in 0...Int(numberOfBeats * viewModel.divisor) {
-                        let vOffset = viewModel.zoom * viewModel.offset + CGFloat(index) * subBeatSpacing
-                        path.move(to: CGPoint(x: vOffset, y: 0))
-                        path.addLine(to: CGPoint(x: vOffset, y: height))
-                    }
-                }
-                .stroke(.blue)
-
-                Path { path in
-                    for index in 0...Int(numberOfBeats) {
-                        let mainBeatOffset = viewModel.zoom * viewModel.offset + CGFloat(index) * mainBeatSpacing
-
-                        path.move(to: CGPoint(x: mainBeatOffset, y: 0))
-                        path.addLine(to: CGPoint(x: mainBeatOffset, y: height))
-                    }
-                }
-                .stroke(.white)
-
-                ForEach(viewModel.hitObjects, id: \.id) { hitObject in
-                    Circle()
-                        .fill(.white)
-                        .offset(x: (viewModel.offset + hitObject.beat) * viewModel.zoom - 20)
-                }
-            }
-            .background(.red)
-            .frame(width: viewModel.zoom * player.duration, height: 40)
-            .offset(x: viewModel.zoom * (player.duration / 2 - viewModel.sliderValue))
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        viewModel.sliderValue = player.currentTime - gesture.translation.width / viewModel.zoom
-                        viewModel.isEditing = true
-                        player.pause()
-                    }
-                    .onEnded { _ in
-                        player.currentTime = viewModel.sliderValue
-                        viewModel.isEditing = false
-                        player.play()
-                    }
-            )
-            Rectangle()
-                .foregroundColor(.blue)
-                .frame(width: 5, height: 60)
-        }
-        .padding()
-    }
-
-    func renderPlaybackButtons(player: AVAudioPlayer) -> some View {
-        HStack {
-            PlaybackControlButtonView(systemName: player.isPlaying ? "pause.circle.fill" : "play.circle.fill",
-                                      fontSize: 44) {
-                viewModel.audioManager.togglePlayer()
-            }
-
-            PlaybackControlButtonView(systemName: "plus.circle.fill") {
-                viewModel.audioManager.increasePlaybackRate()
-            }
-
-            PlaybackControlButtonView(systemName: "minus.circle.fill") {
-                viewModel.audioManager.decreasePlaybackRate()
-            }
-        }
-    }
-
-    func renderHitObject(_ hitObject: any HitObject) -> some View {
-        let absoluteTime = hitObject.beat - viewModel.sliderValue + viewModel.offset
-        return ZStack {
-            Circle()
-                .strokeBorder(.white, lineWidth: 4)
-                .frame(width: min(800, max(100, 100 + 200 * absoluteTime)),
-                       height: min(800, max(100, 100 + 200 * absoluteTime)))
-                .position(x: hitObject.position.x,
-                          y: hitObject.position.y)
-
-            Circle()
-                .fill(.white)
-                .frame(width: 100, height: 100)
-                .position(x: hitObject.position.x,
-                          y: hitObject.position.y) // TODO: constants
-
-        }
-        .opacity(max(0, 1 - 0.5 * abs(absoluteTime)))
+        .environmentObject(viewModel)
     }
 }
 
---
+++ PulsePace/BeatmapDesigner/Views/CanvasView.swift
@@ -0,0 +1,57 @@
+//
+//  CanvasView.swift
+//  PulsePace
+//
+//  Created by Peter Jung on 2023/03/15.
+//
+
+import SwiftUI
+
+struct CanvasView: View {
+    @EnvironmentObject var beatmapDesigner: BeatmapDesignerViewModel
+
+    var body: some View {
+        ZStack {
+            ForEach(beatmapDesigner.hitObjects, id: \.id) { hitObject in
+                renderHitObject(hitObject)
+            }
+        }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .topLeading
+        )
+        .background(.black)
+        .onTapGesture { position in
+            let interval = 1 / (beatmapDesigner.bps * beatmapDesigner.divisor)
+            let beat = ((beatmapDesigner.sliderValue - beatmapDesigner.offset) / interval).rounded()
+            beatmapDesigner.hitObjects.append(TapHitObject(position: position, beat: beat * interval))
+        }
+    }
+
+    private func renderHitObject(_ hitObject: any HitObject) -> some View {
+        let absoluteTime = hitObject.beat - beatmapDesigner.sliderValue + beatmapDesigner.offset
+        return ZStack {
+            Circle()
+                .strokeBorder(.white, lineWidth: 4)
+                .frame(width: min(800, max(100, 100 + 200 * absoluteTime)),
+                       height: min(800, max(100, 100 + 200 * absoluteTime)))
+                .position(x: hitObject.position.x,
+                          y: hitObject.position.y)
+
+            Circle()
+                .fill(.white)
+                .frame(width: 100, height: 100)
+                .position(x: hitObject.position.x,
+                          y: hitObject.position.y) // TODO: constants
+
+        }
+        .opacity(max(0, 1 - 0.5 * abs(absoluteTime)))
+    }
+}
+
+struct CanvasView_Previews: PreviewProvider {
+    static var previews: some View {
+        CanvasView()
+    }
+}
