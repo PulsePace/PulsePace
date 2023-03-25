8a68dededdd6eeb0cc7f2f0c0b553a22e6fa79ae
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-26 03:23:56 +0800

    Add firebase core in main app

--- PulsePace/PulsePaceApp.swift
+++ PulsePace/PulsePaceApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct PulsePaceApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             MenuView()
@@ -16,3 +19,13 @@ struct PulsePaceApp: App {
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions
+                   launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+   
