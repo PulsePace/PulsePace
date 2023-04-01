69aeca57ee65df57be104881fed5fcf1b636f707
Nisemono <james041200@gmail.com>
2023-04-02 06:49:27 +0800

    Dynamic game modes in view

--- PulsePace/Multiplayer/ViewModels/GameModesViewModel.swift
+++ PulsePace/Multiplayer/ViewModels/GameModesViewModel.swift
@@ -8,10 +8,5 @@
 import Foundation
 
 class GameModesViewModel: ObservableObject {
-    var gameModes: [GameMode] = [
-        GameMode(image: "", category: "Singleplayer", title: "Classic Mode",
-                 caption: "Tap, Slide, Hold, Win!", page: Page.playPage),
-        GameMode(image: "", category: "Multiplayer", title: "Beat-Off",
-                 caption: "Battle your friends with rhythm and strategy!", page: Page.lobbyPage)
-    ]
+    var gameModes: [GameMode] = ModeFactory.getAllModes()
 }
