2f4fcba066aee1af77c27036cb5fe921433701fb
Charisma Kausar <charisma.kausar@gmail.com>
2023-04-09 23:33:30 +0800

    Switch AVAudioPlayer to AVAudioEngine

--- PulsePace/BeatmapDesigner/Managers/AudioManager.swift
+++ PulsePace/BeatmapDesigner/Managers/AudioManager.swift
@@ -6,59 +6,59 @@
 //
 
 import Foundation
-import AVKit
+import AVFoundation
 
-final class AudioManager: ObservableObject {
-    @Published var player: AVAudioPlayer?
+final class AudioManager {
+    static let shared = AudioManager()
+    
+    private let audioEngine = AVAudioEngine()
+    private var musicPlayer: AVAudioPlayerNode?
+    private var soundEffectPlayer: AVAudioPlayerNode?
+    
+    private var musicTracks: [String: AVAudioPlayerNode] = [:]
 
-    func startPlayer(track: String) {
+    func playMusic(track: String) {
         guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
             print("Resource not found: \(track)")
             return
         }
-
-        do {
-            stopPlayer()
-            player = try AVAudioPlayer(contentsOf: url)
-            initialisePlayer(player: player)
-        } catch {
-            print("Failed to initialise player", error)
+        guard let player = createAudioPlayerNode(for: url) else {
+            return
         }
+        stopMusic()
+        musicPlayer = player
+        audioEngine.connect(player, to: audioEngine.mainMixerNode, format: nil)
+        player.play()
+    }
+    
+    func stopMusic() {
+        musicPlayer?.stop()
+        audioEngine.disconnectNodeOutput(musicPlayer!)
+        musicPlayer = nil
     }
 
     func increasePlaybackRate() {
-        guard let player = player else {
+        guard let player = musicPlayer else {
             return
         }
         player.rate = min(1, player.rate + 0.25)
     }
 
     func decreasePlaybackRate() {
-        guard let player = player else {
+        guard let player = musicPlayer else {
             return
         }
         player.rate = max(0.25, player.rate - 0.25)
     }
-
-    private func initialisePlayer(player: AVAudioPlayer?) {
-        guard let player = player else {
-            return
+    
+    private func setPlaybackRate(to rate: Float) {
+        if let player = musicPlayer {
+            player.rate = rate
         }
-        player.enableRate = true
-        player.prepareToPlay()
-        player.rate = 1
-        player.play()
     }
 
-    func stopPlayer() {
-        guard let player = player else {
-            return
-        }
-        player.stop()
-    }
-
-    func togglePlayer() {
-        guard let player = player else {
+    func toggleMusic() {
+        guard let player = musicPlayer else {
             return
         }
         if player.isPlaying {
@@ -66,5 +66,23 @@ final class AudioManager: ObservableObject {
         } else {
             player.play()
         }
+    }
+    
+    private func createAudioPlayerNode(for url: URL) -> AVAudioPlayerNode? {
+        guard let audioFile = try? AVAudioFile(forReading: url) else {
+            return nil
+        }
+        let audioFormat = audioFile.processingFormat
+        let audioFrameCount = UInt32(audioFile.length)
+        guard let buffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: audioFrameCount) else {
+            return nil
+        }
+        try? audioFile.read(into: buffer)
+        let player = AVAudioPlayerNode()
+        player.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
+        audioEngine.attach(player)
+        audioEngine.prepare()
+        try? audioEngine.start()
+        return player
     }
 }
